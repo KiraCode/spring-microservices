0fc6cbec222bd62ce00d9ff07050c27fa60106af
KiraCode <tejasdj.5873@gmail.com>
2024-09-09 21:07:51 +0530

    @PathVariable example

--- restful-web-services/src/main/java/com/restweb/services/helloworldcontroller/HelloWorldController.java
+++ restful-web-services/src/main/java/com/restweb/services/helloworldcontroller/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.restweb.services.helloworldcontroller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,5 +22,10 @@ public class HelloWorldController {
 	@GetMapping("/hello-world-bean")
 	public HelloWorldBean helloworldbean() {
 		return new HelloWorldBean("Hello World Message");
+	}
+
+	@GetMapping("/hello-world-bean/path-variable/{name}")
+	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+		return new HelloWorldBean(String.format("Hello world %s", name));
 	}
 }
