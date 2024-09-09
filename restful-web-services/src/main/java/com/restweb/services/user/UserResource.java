5416785a09c1face3c87e57e330a54cf189f61f0
KiraCode <tejasdj.5873@gmail.com>
2024-09-09 21:49:41 +0530

    retriveAllUsers and retrieveOne User

---
+++ restful-web-services/src/main/java/com/restweb/services/user/UserResource.java
@@ -0,0 +1,29 @@
+package com.restweb.services.user;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserResource {
+
+//	@Autowired
+	private UserDaoService service;
+	
+	private UserResource(UserDaoService service) {
+		this.service = service;
+	}
+	
+	@GetMapping("/users")
+	public List<User> retrieveAllUsers(){
+		return service.findAll();
+	}
+	
+	@GetMapping("/users/{id}")
+	public User retrieveUser(@PathVariable Integer id){
+		return service.findOne(id);
+	}
+}
