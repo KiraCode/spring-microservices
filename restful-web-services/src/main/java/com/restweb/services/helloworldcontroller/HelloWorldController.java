66c898649d1765804eb95b9d93dec257934b27f2
KiraCode <tejasdj.5873@gmail.com>
2024-09-09 20:05:36 +0530

    request mapping example rest api

---
+++ restful-web-services/src/main/java/com/restweb/services/helloworldcontroller/HelloWorldController.java
@@ -0,0 +1,16 @@
+package com.restweb.services.helloworldcontroller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+	@RequestMapping(method = RequestMethod.GET, path = "/req-hello-world")
+	public String reqSayHelloWorld() {
+		return"RequestMapping Hello World!!";
+	}
+	
+}
