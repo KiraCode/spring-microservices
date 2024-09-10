649abeba03f1f11cdc947238483605f1e1908f8f
KiraCode <tejasdj.5873@gmail.com>
2024-09-10 19:10:24 +0530

    response entity added so we can see that request is successfull

--- restful-web-services/src/main/java/com/restweb/services/user/UserResource.java
+++ restful-web-services/src/main/java/com/restweb/services/user/UserResource.java
@@ -1,36 +1,45 @@
 package com.restweb.services.user;
 
+import java.net.URI;
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class UserResource {
 
 //	@Autowired
 	private UserDaoService service;
-	
+
 	private UserResource(UserDaoService service) {
 		this.service = service;
 	}
-	
+
 	@GetMapping("/users")
-	public List<User> retrieveAllUsers(){
+	public List<User> retrieveAllUsers() {
 		return service.findAll();
 	}
-	
+
 	@GetMapping("/users/{id}")
-	public User retrieveUser(@PathVariable Integer id){
+	public User retrieveUser(@PathVariable Integer id) {
 		return service.findOne(id);
 	}
-	
+
 	@PostMapping("/users")
-	public void createUsers(@RequestBody User user) {
-		service.save(user);
+	public ResponseEntity<User> createUsers(@RequestBody User user) {
+		User savedUser = service.save(user);
+
+		URI location = ServletUriComponentsBuilder
+				.fromCurrentRequestUri()
+				.path("/{id}")
+				.buildAndExpand(savedUser.getId()).toUri();
+
+		return ResponseEntity.created(location).build();
 	}
 }
