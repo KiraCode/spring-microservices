dd01f3e8827ed6c878851720743f608b6c658659
KiraCode <tejasdj.5873@gmail.com>
2024-09-11 11:58:48 +0530

    using media type version

--- restful-web-services/src/main/java/com/restweb/services/versioning/VersioningPersonController.java
+++ restful-web-services/src/main/java/com/restweb/services/versioning/VersioningPersonController.java
@@ -35,4 +35,14 @@ public class VersioningPersonController {
 	public PersonV2 getSecondVersionofPersonHeaderRequest() {
 		return new PersonV2(new Name("Bob", "Charlie"));
 	}
+	
+	@GetMapping(path = "/person/accept", produces = "application/vnd.company.app-v1+json")
+	public PersonV1 getFirstVersionofPersonAcceptHeader() {
+		return new PersonV1("Bob", "Charlie");
+	}
+	
+	@GetMapping(path = "/person/accept", produces = "application/vnd.company.app-v2+json")
+	public PersonV2 getSecondVersionofPersonAcceptHeader() {
+		return new PersonV2(new Name("Bob", "Charlie"));
+	}
 }
