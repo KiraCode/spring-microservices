9b5041998aaa7df0b1b36c16d525318fb33b7521
KiraCode <tejasdj.5873@gmail.com>
2024-09-18 06:21:50 +0530

    commitinh the eureka server project nanming changes

--- Microservices/naming-server/src/main/java/com/example/eureka/NamingServerApplication.java
+++ Microservices/naming-server/src/main/java/com/example/eureka/NamingServerApplication.java
@@ -2,14 +2,12 @@ package com.example.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaServer
-public class NaminServerApplication {
+public class NamingServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(NaminServerApplication.class, args);
+		SpringApplication.run(NamingServerApplication.class, args);
 	}
 
 }
