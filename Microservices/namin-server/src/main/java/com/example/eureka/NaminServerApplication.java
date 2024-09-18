211c4f8ce873b6c8f67e9330a53288aadfed5245
KiraCode <tejasdj.5873@gmail.com>
2024-09-18 05:54:08 +0530

    setting up Eureka naming sever

---
+++ Microservices/namin-server/src/main/java/com/example/eureka/NaminServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaminServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaminServerApplication.class, args);
+	}
+
+}
